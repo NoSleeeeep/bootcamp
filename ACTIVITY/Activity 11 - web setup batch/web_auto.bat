c4c49fc2eb44fbf8680c59af7469c38b4d3ca0fc
NoSleeeeep <118041688+NoSleeeeep@users.noreply.github.com>
2023-01-12 12:00:22 +0800

    added Activity files and playground files

---
+++ ACTIVITY/Activity 11 - web setup batch/web_auto.bat
@@ -0,0 +1,26 @@
+@echo off
+
+title Windows setup
+echo Setting up web server directories..
+mkdir webpage
+cd webpage
+mkdir css
+cd css 
+type nul > design.css
+cd ..
+mkdir js
+cd js
+type nul > slideshow.js
+type nul > gallery.js
+cd ..
+type nul > about.html
+type nul > shop.html
+echo "<html><title>Test</title><body><h1>Batch run successful!</h1></body></html>" > index.html
+index.html
+echo Checking internet connection..
+ping google.com
+echo Saving your network info..
+ipconfig > network.txt
+echo done
+echo %date% >> run_instances.txt
+pause
